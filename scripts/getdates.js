097b0441b1bbf7e188c1d4ef601ab3bd8449bda5
dianaTafolla510 <dianatafolla510@gmail.com>
2025-01-17 22:10:39 -0800

    lastMod

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,5 +1,16 @@
 //select DOM elements for output
 
 const year = document.querySelector("#year");
+const today = new Date();
 
-let currentyear = d.getFullYear();
+year.innerHTML = `&copy <span class="currentYear">${today.getFullYear()}</span>`;
+
+// Last modification
+
+//alert(document.lastModified);
+//let oLastModif = new Date(document.lastModified);
+
+const date = new Date(document.lastModified);
+document.getElementById("lastModified").innerHTML = date;
+
+
